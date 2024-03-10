644bc2d6a81673d61a6abc27a6827c12533704df
harsh1366217 <146430509+harsh1366217@users.noreply.github.com>
2024-03-10 06:31:14 +0530

    Update frontend.py

--- main/plugins/frontend.py
+++ main/plugins/frontend.py
@@ -17,7 +17,7 @@ ft = f"To use this bot you've to join @{fs}."
 
 message = "Send me the message link you want to start saving from, as a reply to this message."
 
-@Drone.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
+.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def clone(event):
     if event.is_reply:
         reply = await event.get_reply_message()
